39a8e09b8e5f3618dd7cce54184d3488de33f6dd
Larry Taylor and Max Scher <max@>
2017-06-01 11:40:50 -0700

    add js function to scripts.js to reveal turtle div

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#turtles").click(function() {
+    if (operate) {
+      $(".turtles").show();
+    }
+  });
+  
+  };
+
+  // $("#snakes").click(function() {
+  // $(".snakes").show();
+  // $("#insects").click(function() {
+  // $(".insects").show();
+  // $("#oh-my").click(function() {
+  // $(".oh-my").show();
